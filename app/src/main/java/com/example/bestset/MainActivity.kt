5dbbb39623d212b8b6d319cc8cee91f8eb5452e2
kennethastewart <kennethastewart@gmail.com>
2020-05-17 11:45:39 +0100

    fragment shell created for recording weigh. navigation can be done from the drawer

--- app/src/main/java/com/example/bestset/MainActivity.kt
+++ app/src/main/java/com/example/bestset/MainActivity.kt
@@ -42,7 +42,7 @@ class MainActivity : AppCompatActivity() {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.nav_home), drawerLayout)
+                R.id.nav_home, R.id.nav_weight), drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
         viewModel.authenticationState.observe(this, Observer { authenticationState ->
---
+++ app/src/main/java/com/example/bestset/ui/records/RecordWeightFragment.kt
@@ -0,0 +1,20 @@
+package com.example.bestset.ui.records
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class RecordWeightFragment : Fragment(){
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+
+
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
